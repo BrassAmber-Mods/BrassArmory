c975691beee808bb1a5a08e7658475543ad9465d
DerToaster98 <seiler.oli@web.de>
2021-11-27 11:47:12 +0100

    add-in reach modifier for halberds

--- src/main/java/com/milamber_brass/brass_armory/item/HalberdItem.java
+++ src/main/java/com/milamber_brass/brass_armory/item/HalberdItem.java
@@ -1,11 +1,11 @@
 package com.milamber_brass.brass_armory.item;
 
+import javax.annotation.Nonnull;
+
 import com.google.common.collect.Multimap;
-import com.milamber_brass.brass_armory.util.Constants;
 
 import net.minecraft.entity.ai.attributes.Attribute;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
-import net.minecraft.entity.ai.attributes.AttributeModifier.Operation;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.IItemTier;
@@ -15,11 +15,17 @@ import net.minecraft.item.UseAction;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.world.World;
-import net.minecraftforge.common.ForgeMod;
 
-public class HalberdItem extends SwordItem {
+public class HalberdItem extends SwordItem implements ICustomReachItem {
 
 	private final double reachDistanceBonus;
+	
+	protected Multimap<Attribute, AttributeModifier> reachModifiers = null;
+	
+	public HalberdItem(IItemTier tier, int attackDamageIn, Properties builderIn) {
+        this(tier, attackDamageIn, builderIn, 2.0D);
+    }
+
 	
     public HalberdItem(IItemTier tier, int attackDamageIn, Properties builderIn, double reachDistanceBonus) {
         super(tier, attackDamageIn, -3.4F, builderIn);
@@ -43,15 +49,33 @@ public class HalberdItem extends SwordItem {
     public int getUseDuration(ItemStack p_77626_1_) {
     	return 72000;
     }
-    
-    @Override
-    public Multimap<Attribute, AttributeModifier> getAttributeModifiers(EquipmentSlotType slot, ItemStack stack) {
-    	Multimap<Attribute, AttributeModifier> multimap = super.getAttributeModifiers(slot, stack);
 
-		if (slot == EquipmentSlotType.MAINHAND) {
-			multimap.put(ForgeMod.REACH_DISTANCE.get(), new AttributeModifier(Constants.REACH_DISTANCE_MODIFIER, "Weapon modifier", this.reachDistanceBonus, Operation.ADDITION));
-		}
-    	return super.getAttributeModifiers(slot, stack);
-    }
+	@Override
+	public double getReachDistanceBonus() {
+		return this.reachDistanceBonus;
+	}
+
+	@Override
+	public Multimap<Attribute, AttributeModifier> getAttributeModifiers(EquipmentSlotType slot, ItemStack stack) {
+		return ICustomReachItem.super.getAttributeModifiers(slot, stack);
+	}
+	
+	@Nonnull
+	@Override
+	public Multimap<Attribute, AttributeModifier> execSuperGetAttributeModifiers(EquipmentSlotType slot, ItemStack stack) {
+		return super.getAttributeModifiers(slot, stack);
+	}
+
+
+	@Override
+	public Multimap<Attribute, AttributeModifier> getCustomAttributesField() {
+		return this.reachModifiers;
+	}
+
+
+	@Override
+	public void setCustomAttributesField(Multimap<Attribute, AttributeModifier> value) {
+		this.reachModifiers = value;
+	}
     
 }
