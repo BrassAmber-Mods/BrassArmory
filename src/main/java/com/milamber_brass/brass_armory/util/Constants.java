862d697efd14124c5e46cb4fc2d26145d0f87e83
DerToaster98 <seiler.oli@web.de>
2021-11-27 11:47:02 +0100

    update implementation to fit forge

--- src/main/java/com/milamber_brass/brass_armory/util/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.milamber_brass.brass_armory.util;
-
-import java.util.UUID;
-
-public class Constants {
-	
-	public static final UUID REACH_DISTANCE_MODIFIER = UUID.fromString("95dd73a8-c715-42f9-8f6d-abf5e40fa3cd");
-
-}
