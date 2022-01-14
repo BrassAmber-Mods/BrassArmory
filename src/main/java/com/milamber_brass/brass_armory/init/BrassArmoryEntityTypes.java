214646f611eb211280e741dca54bad80959e62fa
DerToaster98 <seiler.oli@web.de>
2022-01-14 16:52:26 +0100

    register thrown weapon entities

--- src/main/java/com/milamber_brass/brass_armory/init/BrassArmoryEntityTypes.java
+++ src/main/java/com/milamber_brass/brass_armory/init/BrassArmoryEntityTypes.java
@@ -2,7 +2,10 @@ package com.milamber_brass.brass_armory.init;
 
 import com.milamber_brass.brass_armory.BrassArmory;
 import com.milamber_brass.brass_armory.entity.SpearEntity;
+import com.milamber_brass.brass_armory.entity.ThrownBattleAxeEntity;
+import com.milamber_brass.brass_armory.entity.ThrownDaggerEntity;
 import com.milamber_brass.brass_armory.entity.projectile.BAArrowEntity;
+
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityClassification;
 import net.minecraft.entity.EntityType;
@@ -17,10 +20,22 @@ public class BrassArmoryEntityTypes {
     public static final DeferredRegister<EntityType<?>> ENTITY_TYPES = DeferredRegister.create(ForgeRegistries.ENTITIES, BrassArmory.MOD_ID);
 
     public static final RegistryObject<EntityType<SpearEntity>> SPEAR =
-            ENTITY_TYPES.register("wood_spear", () -> EntityType.Builder
-                    .<SpearEntity>of(SpearEntity::new, EntityClassification.MISC).sized(1.0f, 2.0f)
+            ENTITY_TYPES.register("thrown_spear", () -> EntityType.Builder
+                    .<SpearEntity>of(SpearEntity::new, EntityClassification.MISC).sized(0.5f, 0.5f)
                     .clientTrackingRange(64).updateInterval(20)
-                    .build(new ResourceLocation(BrassArmory.MOD_ID, "item/wood_spear").toString()));
+                    .build(new ResourceLocation(BrassArmory.MOD_ID, "thrown_spear").toString()));
+    
+    public static final RegistryObject<EntityType<ThrownDaggerEntity>> DAGGER =
+            ENTITY_TYPES.register("thrown_dagger", () -> EntityType.Builder
+                    .<ThrownDaggerEntity>of(ThrownDaggerEntity::new, EntityClassification.MISC).sized(0.5f, 0.5f)
+                    .clientTrackingRange(64).updateInterval(20)
+                    .build(new ResourceLocation(BrassArmory.MOD_ID, "thrown_dagger").toString()));
+    
+    public static final RegistryObject<EntityType<ThrownBattleAxeEntity>> BATTLEAXE =
+            ENTITY_TYPES.register("thrown_battleaxe", () -> EntityType.Builder
+                    .<ThrownBattleAxeEntity>of(ThrownBattleAxeEntity::new, EntityClassification.MISC).sized(0.5f, 0.5f)
+                    .clientTrackingRange(64).updateInterval(20)
+                    .build(new ResourceLocation(BrassArmory.MOD_ID, "thrown_battleaxe").toString()));
 
     public static final RegistryObject<EntityType<BAArrowEntity>> BA_ARROW = registerEntityType("ba_arrow",
             EntityType.Builder.<BAArrowEntity>of(BAArrowEntity::new, EntityClassification.MISC)
