4e29559080b2bec6a39551bfb09282b9a9b257c2
DerToaster98 <seiler.oli@web.de>
2022-01-14 16:20:14 +0100

    create base class for thrown weapons

---
+++ src/main/java/com/milamber_brass/brass_armory/entity/ThrowableWeaponProjectileBase.java
@@ -0,0 +1,188 @@
+package com.milamber_brass.brass_armory.entity;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import javax.annotation.ParametersAreNonnullByDefault;
+
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.AbstractArrowEntity;
+import net.minecraft.entity.projectile.TridentEntity;
+import net.minecraft.item.IItemTier;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundEvent;
+import net.minecraft.util.SoundEvents;
+import net.minecraft.util.math.EntityRayTraceResult;
+import net.minecraft.util.math.vector.Vector3d;
+import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+public abstract class ThrowableWeaponProjectileBase extends TridentEntity {
+
+    public int returningTicks;
+    public IItemTier finalTier;
+    protected ItemStack thrownStack;
+    protected boolean dealtDamage;
+
+    public ThrowableWeaponProjectileBase(EntityType<? extends ThrowableWeaponProjectileBase> type, World worldIn) {
+        super(type, worldIn);
+    }
+
+    public ThrowableWeaponProjectileBase(EntityType<? extends ThrowableWeaponProjectileBase> type, World worldIn, LivingEntity thrower, ItemStack thrownStackIn, IItemTier tier) {
+        super(type, worldIn);
+        this.setOwner(thrower);
+        if(thrower instanceof PlayerEntity) {
+        	this.pickup = PickupStatus.ALLOWED;
+        }
+        this.tridentItem = thrownStackIn;
+        this.thrownStack = thrownStackIn.copy();
+        finalTier = tier;
+
+    }
+
+    public ThrowableWeaponProjectileBase(EntityType<? extends ThrowableWeaponProjectileBase> type, World worldIn, double x, double y, double z) {
+    	 super(type, worldIn);
+    	 this.setPos(x, y, z);
+    }
+
+
+    public abstract ResourceLocation getTierResourceLocation();
+
+
+    protected void defineSynchedData() {
+        super.defineSynchedData();
+    }
+
+    /**
+     * Called to update the entity's position/logic.
+     */
+    public void tick() {
+        if (this.inGroundTime > 4) {
+            this.dealtDamage = true;
+        }
+
+        Entity entity = this.getOwner();
+        if ((this.dealtDamage || this.isNoPhysics()) && entity != null) {
+            if (!this.level.isClientSide && this.pickup == AbstractArrowEntity.PickupStatus.ALLOWED) {
+                this.spawnAtLocation(this.getPickupItem(), 0.1F);
+            }
+            this.remove();
+        }
+        super.tick();
+    }
+
+    @Nonnull
+    protected ItemStack getPickupItem() {
+        return this.thrownStack.copy();
+    }
+
+    /**
+     * Gets the EntityRayTraceResult representing the entity hit
+     */
+    @Nullable
+    @ParametersAreNonnullByDefault
+    protected EntityRayTraceResult findHitEntity(Vector3d startVec, Vector3d endVec) {
+        return this.dealtDamage ? null : super.findHitEntity(startVec, endVec);
+    }
+
+    /**
+     * Called when the arrow hits an entity
+     */
+    protected void onHitEntity(EntityRayTraceResult result) {
+        Entity entity = result.getEntity();
+        float f = 8.0F;
+        if (entity instanceof LivingEntity) {
+            LivingEntity livingentity = (LivingEntity) entity;
+            f += EnchantmentHelper.getDamageBonus(this.thrownStack, livingentity.getMobType());
+        }
+
+        Entity entity1 = this.getOwner();
+        DamageSource damagesource = DamageSource.trident(this, entity1 == null ? this : entity1);
+        this.dealtDamage = true;
+        SoundEvent soundevent = SoundEvents.TRIDENT_HIT;
+        if (entity.hurt(damagesource, f)) {
+            if (entity.getType() == EntityType.ENDERMAN) {
+                return;
+            }
+
+            if (entity instanceof LivingEntity) {
+                LivingEntity livingentity1 = (LivingEntity) entity;
+                if (entity1 instanceof LivingEntity) {
+                    EnchantmentHelper.doPostHurtEffects(livingentity1, entity1);
+                    EnchantmentHelper.doPostDamageEffects((LivingEntity) entity1, livingentity1);
+                }
+
+                this.doPostHurtEffects(livingentity1);
+            }
+        }
+
+        this.setDeltaMovement(this.getDeltaMovement().multiply(-0.01D, -0.1D, -0.01D));
+        float f1 = 1.0F;
+        this.playSound(soundevent, f1, 1.0F);
+    }
+
+    /**
+     * The sound made when an entity is hit by this projectile
+     */
+    @Nonnull
+    protected SoundEvent getDefaultHitGroundSoundEvent() {
+        return SoundEvents.TRIDENT_HIT_GROUND;
+    }
+
+    /**
+     * Called by a player entity when they collide with an entity
+     */
+    @ParametersAreNonnullByDefault
+    public void playerTouch(PlayerEntity entityIn) {
+        Entity entity = this.getOwner();
+        if (entity == null || entity.getUUID() == entityIn.getUUID()) {
+            super.playerTouch(entityIn);
+        }
+    }
+
+    /**
+     * (abstract) Protected helper method to read subclass entity data from NBT.
+     */
+    @ParametersAreNonnullByDefault
+    public void readAdditionalSaveData(CompoundNBT compound) {
+        super.readAdditionalSaveData(compound);
+        if (compound.contains("Spear", 10)) {
+            this.thrownStack = ItemStack.of(compound.getCompound("Spear"));
+        }
+
+        this.dealtDamage = compound.getBoolean("DealtDamage");
+    }
+
+    @ParametersAreNonnullByDefault
+    public void addAdditionalSaveData(CompoundNBT compound) {
+        super.addAdditionalSaveData(compound);
+        compound.put("Spear", this.thrownStack.save(new CompoundNBT()));
+        compound.putBoolean("DealtDamage", this.dealtDamage);
+    }
+
+    public void tickDespawn() {
+        if (this.pickup != AbstractArrowEntity.PickupStatus.ALLOWED) {
+            super.tickDespawn();
+        }
+
+    }
+
+    protected float getWaterInertia() {
+        return 0.99F;
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    public boolean shouldRender(double x, double y, double z) {
+        return true;
+    }
+
+}
+
+
