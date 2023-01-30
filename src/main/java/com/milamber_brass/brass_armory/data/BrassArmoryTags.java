82b0990d88adec2fdd59bc973f2266b0686a104d
jodi / jodlodi <jodlodi090@gmail.com>
2023-01-30 20:35:44 +0100

    Added BA items to proper recipe book pages
    The only reason this update is so big is cuz I had to run datagen on 1.19.2, not much actually changed

--- src/main/java/com/milamber_brass/brass_armory/data/BrassArmoryTags.java
+++ src/main/java/com/milamber_brass/brass_armory/data/BrassArmoryTags.java
@@ -45,6 +45,11 @@ public abstract class BrassArmoryTags {
         public static final TagKey<Item> BLEEDING_EDGE = TagKey.create(Registry.ITEM_REGISTRY, BrassArmory.locate("bleeding_edge"));
         public static final TagKey<Item> WARP_CRYSTAL_GENERATORS = TagKey.create(Registry.ITEM_REGISTRY, BrassArmory.locate("warp_crystal_generators"));
 
+        public static final TagKey<Item> BUILDING_BLOCKS = TagKey.create(Registry.ITEM_REGISTRY, BrassArmory.locate("building_blocks"));
+        public static final TagKey<Item> REDSTONE = TagKey.create(Registry.ITEM_REGISTRY, BrassArmory.locate("redstone"));
+        public static final TagKey<Item> MISC = TagKey.create(Registry.ITEM_REGISTRY, BrassArmory.locate("misc"));
+        public static final TagKey<Item> EQUIPMENT = TagKey.create(Registry.ITEM_REGISTRY, BrassArmory.locate("equipment"));
+
         public Items(DataGenerator generator, BlockTagsProvider blockTagsProvider, String modId, @Nullable ExistingFileHelper existingFileHelper) {
             super(generator, blockTagsProvider, modId, existingFileHelper);
         }
@@ -136,6 +141,109 @@ public abstract class BrassArmoryTags {
                     BrassArmoryItems.GOLDEN_HALBERD.get(),
                     BrassArmoryItems.GOLDEN_MACE.get(),
                     BrassArmoryItems.GOLDEN_SPEAR.get()
+            );
+
+            tag(MISC).add(
+                    BrassArmoryBlocks.EXPLORERS_ROPE_BLOCK.get().asItem(),
+                    BrassArmoryItems.PETTY_WARP_CRYSTAL.get(),
+                    BrassArmoryItems.LESSER_WARP_CRYSTAL.get(),
+                    BrassArmoryItems.COMMON_WARP_CRYSTAL.get(),
+                    BrassArmoryItems.GREATER_WARP_CRYSTAL.get(),
+                    BrassArmoryItems.GRAND_WARP_CRYSTAL.get(),
+                    BrassArmoryItems.GUN_STOCK.get(),
+                    BrassArmoryItems.MUSKET_PARTS.get(),
+                    BrassArmoryItems.BLUNDERBUSS_PARTS.get()
+            );
+
+            tag(EQUIPMENT).add(
+                    BrassArmoryItems.DIRT_ARROW.get(),
+                    BrassArmoryItems.EX_ARROW.get(),
+                    BrassArmoryItems.FROST_ARROW.get(),
+                    BrassArmoryItems.GRASS_ARROW.get(),
+                    BrassArmoryItems.LASER_ARROW.get(),
+                    BrassArmoryItems.ROPE_ARROW.get(),
+                    BrassArmoryItems.SLIME_ARROW.get(),
+                    BrassArmoryItems.WARP_ARROW.get(),
+                    BrassArmoryItems.FIRE_ARROW.get(),
+                    BrassArmoryItems.CONFUSION_ARROW.get(),
+                    BrassArmoryItems.TORCH_ARROW.get(),
+
+                    BrassArmoryItems.WOODEN_BOOMERANG.get(),
+                    BrassArmoryItems.GOLDEN_BOOMERANG.get(),
+                    BrassArmoryItems.STONE_BOOMERANG.get(),
+                    BrassArmoryItems.IRON_BOOMERANG.get(),
+                    BrassArmoryItems.DIAMOND_BOOMERANG.get(),
+                    BrassArmoryItems.NETHERITE_BOOMERANG.get(),
+
+                    BrassArmoryItems.BOMB.get(),
+                    BrassArmoryItems.STICKY_BOMB.get(),
+                    BrassArmoryItems.BOUNCY_BOMB.get(),
+
+                    BrassArmoryItems.WOODEN_SPEAR.get(),
+                    BrassArmoryItems.GOLDEN_SPEAR.get(),
+                    BrassArmoryItems.STONE_SPEAR.get(),
+                    BrassArmoryItems.IRON_SPEAR.get(),
+                    BrassArmoryItems.DIAMOND_SPEAR.get(),
+                    BrassArmoryItems.NETHERITE_SPEAR.get(),
+
+                    BrassArmoryItems.FIRE_ROD.get(),
+
+                    BrassArmoryItems.WOODEN_DAGGER.get(),
+                    BrassArmoryItems.GOLDEN_DAGGER.get(),
+                    BrassArmoryItems.STONE_DAGGER.get(),
+                    BrassArmoryItems.IRON_DAGGER.get(),
+                    BrassArmoryItems.DIAMOND_DAGGER.get(),
+                    BrassArmoryItems.NETHERITE_DAGGER.get(),
+
+                    BrassArmoryItems.WOODEN_BATTLEAXE.get(),
+                    BrassArmoryItems.GOLDEN_BATTLEAXE.get(),
+                    BrassArmoryItems.STONE_BATTLEAXE.get(),
+                    BrassArmoryItems.IRON_BATTLEAXE.get(),
+                    BrassArmoryItems.DIAMOND_BATTLEAXE.get(),
+                    BrassArmoryItems.NETHERITE_BATTLEAXE.get(),
+
+                    BrassArmoryItems.WOODEN_HALBERD.get(),
+                    BrassArmoryItems.GOLDEN_HALBERD.get(),
+                    BrassArmoryItems.STONE_HALBERD.get(),
+                    BrassArmoryItems.IRON_HALBERD.get(),
+                    BrassArmoryItems.DIAMOND_HALBERD.get(),
+                    BrassArmoryItems.NETHERITE_HALBERD.get(),
+
+                    BrassArmoryItems.WOODEN_MACE.get(),
+                    BrassArmoryItems.GOLDEN_MACE.get(),
+                    BrassArmoryItems.STONE_MACE.get(),
+                    BrassArmoryItems.IRON_MACE.get(),
+                    BrassArmoryItems.DIAMOND_MACE.get(),
+                    BrassArmoryItems.NETHERITE_MACE.get(),
+
+                    BrassArmoryItems.WOODEN_SPIKY_BALL.get(),
+                    BrassArmoryItems.GOLDEN_SPIKY_BALL.get(),
+                    BrassArmoryItems.STONE_SPIKY_BALL.get(),
+                    BrassArmoryItems.IRON_SPIKY_BALL.get(),
+                    BrassArmoryItems.DIAMOND_SPIKY_BALL.get(),
+                    BrassArmoryItems.NETHERITE_SPIKY_BALL.get(),
+
+                    BrassArmoryItems.WOODEN_FLAIL.get(),
+                    BrassArmoryItems.GOLDEN_FLAIL.get(),
+                    BrassArmoryItems.STONE_FLAIL.get(),
+                    BrassArmoryItems.IRON_FLAIL.get(),
+                    BrassArmoryItems.DIAMOND_FLAIL.get(),
+                    BrassArmoryItems.NETHERITE_FLAIL.get(),
+
+                    BrassArmoryItems.FLINTLOCK_PISTOL.get(),
+                    BrassArmoryItems.MUSKET.get(),
+                    BrassArmoryItems.BLUNDERBUSS.get(),
+                    BrassArmoryItems.MUSKET_BALL.get(),
+                    BrassArmoryItems.BUNDLE_SHOT.get(),
+                    BrassArmoryItems.CANNON.get(),
+                    BrassArmoryItems.CANNON_BALL.get(),
+                    BrassArmoryItems.CARCASS_ROUND.get(),
+                    BrassArmoryItems.SIEGE_ROUND.get(),
+
+                    BrassArmoryItems.LONGBOW.get(),
+                    BrassArmoryItems.KATANA.get(),
+                    BrassArmoryItems.GLIDER.get(),
+                    BrassArmoryItems.QUIVER.get()
             );
         }
     }
